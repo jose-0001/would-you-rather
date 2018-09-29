35cbed7d11ab4ef25278215b029679b719f1bf5c
jose123 <jose@test.com>
2018-09-29 18:06:22 -0500

    user, questions, authedUser reducers created

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,12 +1,16 @@
+import { getInitialData } from "../utils/_DATA";
 import { receiveUsers } from "../actions/users";
 import { receiveQuestions } from "../actions/questions";
-import { getInitialData } from "../utils/_DATA";
+import { setAuthedUser } from "../actions/authedUser";
+
+const AUTHED_ID = "tylermcginnis";
 
 export function handleInitialData() {
   return dispatch => {
     return getInitialData().then(({ users, questions }) => {
-      dispatch(_getUsers(users));
-      dispatch(_getQuestions(questions));
-    })
+      dispatch(receiveUsers(users));
+      dispatch(receiveQuestions(questions));
+      dispatch(setAuthedUser(AUTHED_ID));
+    });
   };
 }
