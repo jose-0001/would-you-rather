f1bddeb4bc4a3f8a2518e5f9e7dfdab52826daaa
jose123 <jose@test.com>
2018-09-29 18:42:58 -0500

    added exports to each action

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 import DropDownSelection from './DropDownSelection';
 
 class App extends Component {
+  componentDidMount(){
+    this.props.dispatch(handleInitialData());
+  }
   render() {
     return (
       <div>
@@ -11,4 +16,5 @@ class App extends Component {
   }
 }
 
-export default App;
+// to gain access to dispatch we need to connect
+export default connect()(App);
