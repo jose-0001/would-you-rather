0028bf3a8bf84aef8411497c3b8d35eadd4e8bf4
jose123 <jose@test.com>
2018-09-30 15:37:49 -0500

    Dashboard renders if authed user is not null

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
-import DropDownSelection from './DropDownSelection';
+import DropDownSelection from "./DropDownSelection";
 import Dashboard from "./Dashboard";
 
 class App extends Component {
-  componentDidMount(){
+  componentDidMount() {
     this.props.dispatch(handleInitialData());
   }
   render() {
-    return (
-      <div>
-        <Dashboard />
-      </div>
-    );
+    return <div>{this.props.loading === true ? null : <Dashboard />}</div>;
   }
 }
 
+function mapStateToProps({ authedUser }) {
+  return {
+    loading: authedUser === null
+  };
+}
+
 // to gain access to dispatch we need to connect
-export default connect()(App);
+export default connect(mapStateToProps)(App);
