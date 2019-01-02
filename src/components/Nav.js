99c4d2d5d5a788168c7dfdc026a8a30df3ba8eaa
JCFlores <jflores717@gmail.com>
2019-01-01 18:57:19 -0600

    style: adjusted answerTab border radius

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { NavLink, Redirect } from "react-router-dom";
 import { handleLogOut } from "../actions/shared";
