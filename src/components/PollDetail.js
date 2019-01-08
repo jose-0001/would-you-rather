cec0e73eef8e1733f7613ac4efe326ef8cfea733
JCFlores <jflores717@gmail.com>
2019-01-07 20:26:04 -0600

    refactor: show 404 page if poll (/:question_id) does not exist

--- src/components/PollDetail.js
+++ src/components/PollDetail.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 import ViewPoll from "./ViewPoll";
 import UnAnsweredQuestion from "./UnAnsweredQuestion";
 
 function PollDetail({ hasAnswered, optionOne, optionTwo, user, id }) {
+  if (id === false) {
+    return <Redirect to="/404" />;
+  }
   return (
     <div>
       {hasAnswered ? (
@@ -22,19 +26,28 @@ function PollDetail({ hasAnswered, optionOne, optionTwo, user, id }) {
 
 function mapStateToProps({ questions, authedUser, users }, { match }) {
   const id = match.params.question_id.slice(1);
+  const idChecker = Object.values(questions).some(question => {
+    return question.id === id;
+  });
   // question object contains optionOne, optionTwo, author, and id
-  const question = questions[id];
-  const { optionOne, optionTwo } = question;
-  return {
-    authedUser,
-    optionOne,
-    optionTwo,
-    user: users[question.author],
-    id,
-    hasAnswered:
-      optionOne.votes.includes(authedUser.id) ||
-      optionTwo.votes.includes(authedUser.id)
-  };
+  if (idChecker) {
+    const question = questions[id];
+    const { optionOne, optionTwo } = question;
+    return {
+      authedUser,
+      optionOne,
+      optionTwo,
+      user: users[question.author],
+      id,
+      hasAnswered:
+        optionOne.votes.includes(authedUser.id) ||
+        optionTwo.votes.includes(authedUser.id)
+    };
+  } else {
+    return {
+      id: false
+    };
+  }
 }
 
 export default connect(mapStateToProps)(PollDetail);
