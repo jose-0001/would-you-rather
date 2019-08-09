0506666c68caf118f211d4d1ca2e68bb6a6530f9
joseflores-io <jflores717@gmail.com>
2019-08-09 13:29:34 -0500

    Updates

---
+++ precache-manifest.885276a625a9013aaef7909a6d7c8b07.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c9895f1ebd373d3a5af",
+    "url": "/would-you-rather/static/css/main.8e82c6ac.chunk.css"
+  },
+  {
+    "revision": "7c9895f1ebd373d3a5af",
+    "url": "/would-you-rather/static/js/main.695ae248.chunk.js"
+  },
+  {
+    "revision": "3c0bbf8aecd297c26580",
+    "url": "/would-you-rather/static/js/runtime~main.2bbe9c67.js"
+  },
+  {
+    "revision": "8b86bddff83ae7c24718",
+    "url": "/would-you-rather/static/css/2.e0d3a64a.chunk.css"
+  },
+  {
+    "revision": "8b86bddff83ae7c24718",
+    "url": "/would-you-rather/static/js/2.0574022a.chunk.js"
+  },
+  {
+    "revision": "afff988ca46ba42afce5aa1c50d9cafa",
+    "url": "/would-you-rather/static/media/jenny.afff988c.jpg"
+  },
+  {
+    "revision": "432d61fd3f38f54f912047ab678cc0c4",
+    "url": "/would-you-rather/static/media/elliot.432d61fd.jpg"
+  },
+  {
+    "revision": "1652f7e9c330905fa6f55a66ec9b33c3",
+    "url": "/would-you-rather/static/media/justen.1652f7e9.jpg"
+  },
+  {
+    "revision": "046e91f77483a277a434bac9ae4885e2",
+    "url": "/would-you-rather/static/media/matthew.046e91f7.png"
+  },
+  {
+    "revision": "c60fcb0be09e491814a8f4253973a83b",
+    "url": "/would-you-rather/static/media/veronika.c60fcb0b.jpg"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/would-you-rather/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/would-you-rather/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/would-you-rather/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/would-you-rather/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/would-you-rather/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/would-you-rather/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/would-you-rather/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/would-you-rather/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/would-you-rather/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/would-you-rather/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/would-you-rather/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/would-you-rather/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/would-you-rather/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/would-you-rather/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/would-you-rather/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/would-you-rather/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "e0a9ae27be3c2731e512277c43759aa7",
+    "url": "/would-you-rather/index.html"
+  }
+];
